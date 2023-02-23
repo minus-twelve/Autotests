783064e51a5ab7fdbcd097c701b59b29e28159de
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-23 23:09:35 +0300

    Create wait1.py

---
+++ Python/suninjuly.github.io/wait1.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.by import By
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/wait1.html")
+
+    time.sleep(2)
+
+    button = browser.find_element(By.ID, "verify")
+    button.click()
+    message = browser.find_element(By.XPATH, "//div[@id='verify_message']")
+    print(message.text)
+
+ 
+
+finally:
+    time.sleep(2)
+    browser.quit()
