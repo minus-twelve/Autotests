6f3fba69ea28f5a6b2aa4ddeb7a9d16c5297f439
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-25 03:10:07 +0300

    Create test_xfail.py

---
+++ Python/test/test_xfail.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
+
+
+#1 failed, 1 skipped, 1 xfailed in 0.07s
