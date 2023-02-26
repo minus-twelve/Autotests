9f59c2ae37c8ba77e7073c04ac2213b72a02f8d5
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-26 23:07:31 +0300

    Update test_fixture9.py

--- Python/selenium1py.pythonanywhere.com/test_fixture9.py
+++ Python/selenium1py.pythonanywhere.com/test_fixture9.py
@@ -13,7 +13,12 @@ def browser():
     browser.quit()
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+    def test_guest_should_see_navbar_element(self, browser, language):
+        print("test")
