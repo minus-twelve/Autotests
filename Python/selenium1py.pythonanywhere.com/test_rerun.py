da10e3ca8381ac1ff62015200b16a7c28a66c2f0
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-27 01:26:05 +0300

    Create test_rerun.py

---
+++ Python/selenium1py.pythonanywhere.com/test_rerun.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+#pip install pytest-rerunfailures
+#pytest -v --tb=line --reruns 1 --browser_name=chrome test_rerun.py
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
